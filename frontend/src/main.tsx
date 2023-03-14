2c8cf639e4310d34d2e3b9aada4b95e2c2a5b0b4
Thiago Henrique Fonseca Pereira <thiagohp070@gmail.com>
2023-03-14 16:52:42 -0300

    add: home page with react router dom

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,12 +1,40 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import App from './App';
+import Calendar from './components/Calendar';
 
 import './index.css';
+import ErrorPage from './pages/ErrorPage';
+import Detail from './pages/Manager/Detail';
+import Home from './pages/Manager/Home';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <ErrorPage />
+  },
+  {
+    path: 'manager',
+    element: <Home />,
+    children: [
+      {
+        path: '',
+        element: <Calendar />
+      },
+      {
+        path: 'details',
+        element: <Detail />
+      },
+
+    ],
+  }
+]);
 
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 );
